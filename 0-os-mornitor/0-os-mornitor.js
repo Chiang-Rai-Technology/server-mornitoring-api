2f14ab33f7059fc5faea8540d122044ba036fbac
moscow <moscow@168bits.com>
2023-01-18 11:27:08 +0700

    add

--- 0-os-mornitor/0-os-mornitor.js
+++ 0-os-mornitor/0-os-mornitor.js
@@ -1,7 +1,7 @@
 (async function main() {
     await require('./helpers/express')()
         .then(app => (console.log('server is running...'), app))
-        .then(async app => (await require('./controllers/server.controllers')(), app))
+        .then(app => (require('./controllers/server.controllers')(), app))
         .then(app => app.listen())
-        .catch(error => (console.error(error), main()));
+        .catch(error => console.error(error));
 })();
